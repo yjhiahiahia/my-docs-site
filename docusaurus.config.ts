15d4bec3ba240debc130bb8cd5641e488f7a1a53
yangjing <v-yangjing20@xiaomi.com>
2026-07-10 15:27:57 +0800

    添加 CI/CD 自动部署

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,15 +15,16 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://yjhiahiahia.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/my-docs-site/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'yjhiahiahia', // Usually your GitHub org/user name.
+  projectName: 'my-docs-site', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
 
